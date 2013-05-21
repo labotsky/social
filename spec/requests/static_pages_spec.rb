5c1acc16c6788e045b87c11e4f463043f8c582b5
sergey <sergey1993.08@mail.ru>
2013-05-21 12:45:54 +0300

    fixies bags

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -8,6 +8,7 @@ describe "StaticPages" do
 		before {visit root_path}
 		it {should have_content('Social Network')}
 		it {should have_content('Войти')}
-		it {should have_content('Присоединиться')}  	
+		it {should have_content('Присоединиться')} 
+		it {should have_selector('title', :text => 'Social Network')} 	
 	end
 end
